9eae92360991b526184c31ab55a18c8f6572d2d2
takeyamami <task8705@yahoo.co.jp>
2021-07-05 20:33:43 +0900

    update: CORS有効化

--- api/src/main/java/com/example/api/TalkController.java
+++ api/src/main/java/com/example/api/TalkController.java
@@ -3,24 +3,27 @@ package com.example.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TalkController {
 	@Autowired
 	private TalkRepository talkRepository;
-	
-	@GetMapping("talk/list/")
-	public List<Talk> findTalks() {
-		return talkRepository.findAll();
-	}
 
 	@GetMapping("talk/list/{id}")
 	public List<Talk> findTalks(@PathVariable("id") Long rid) {
 		return talkRepository.findByRidOrderByTid(rid);
+	}
+
+	@PostMapping("talk/create")
+	public Talk save(@RequestBody Talk talk) {
+		return talkRepository.save(talk);
 	}
 
 }
