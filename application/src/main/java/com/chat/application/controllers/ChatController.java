8c2b5a8f82ef635da67e22022ab32ef94c3ab994
takeyamami <task8705@yahoo.co.jp>
2021-09-11 16:49:01 +0900

    update: Cookieを利用

--- application/src/main/java/com/chat/application/controllers/ChatController.java
+++ application/src/main/java/com/chat/application/controllers/ChatController.java
@@ -4,13 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.CookieValue;
 
 @Controller
 public class ChatController {
 
     @RequestMapping("/chat")
-    public String chat(ModelMap modelMap) {
-//        modelMap.addAttribute("rid", rid);
+    public String chat(@CookieValue(value="id", required=false) String email, ModelMap modelMap) {
+        modelMap.addAttribute("email", email);
         return "chat";
     }
 }
--- application/src/main/java/com/chat/application/controllers/LoginController.java
+++ application/src/main/java/com/chat/application/controllers/LoginController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -15,10 +17,15 @@ public class LoginController {
     }
 
     @PostMapping("/login/action")
-    public String action(@RequestParam("email")String email,@RequestParam("password")String password, ModelMap modelMap) {
+    public String action(@RequestParam("email")String email,@RequestParam("password")String password, HttpServletResponse response) {
         if (email.equals("") || password.equals("")) {
             return "login";
         }
+
+        Cookie cookie = new Cookie("id", email);
+        cookie.setMaxAge(24 * 60 * 60);
+        cookie.setPath("/");
+        response.addCookie(cookie);
 
         return "redirect:/chat";
     }
