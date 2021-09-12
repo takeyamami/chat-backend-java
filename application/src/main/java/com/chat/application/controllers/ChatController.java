5de4fe825ab4251dd5935e06cd9490f9e2da9216
yamami78651 <task8705@yahoo.co.jp>
2021-09-13 07:14:23 +0900

    方針だけ書き出し

--- application/src/main/java/com/chat/application/controllers/ChatController.java
+++ application/src/main/java/com/chat/application/controllers/ChatController.java
@@ -3,7 +3,6 @@ package com.chat.application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CookieValue;
 
 @Controller
@@ -12,6 +11,9 @@ public class ChatController {
     @RequestMapping("/chat")
     public String chat(@CookieValue(value="id", required=false) String email, ModelMap modelMap) {
         modelMap.addAttribute("email", email);
+
+        // Modelsを実装して、ダミーリストを作って画面に表示
+
         return "chat";
     }
 }
