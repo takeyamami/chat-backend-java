9de6c2d07aa8cf531ef0f84db54b86a84e2cf86d
yamami78651 <task8705@yahoo.co.jp>
2021-09-13 21:24:18 +0900

    add: フォームからのリクエストを受け取る

--- application/src/main/java/com/chat/application/controllers/ChatController.java
+++ application/src/main/java/com/chat/application/controllers/ChatController.java
@@ -3,7 +3,9 @@ package com.chat.application.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChatController {
@@ -15,5 +17,11 @@ public class ChatController {
         // Modelsを実装して、ダミーリストを作って画面に表示
 
         return "chat";
+    }
+
+    @PostMapping("/chat/action")
+    public String action(@RequestParam("rid")Integer rid,@RequestParam("message")String message,ModelMap modelMap) {
+
+        return "redirect:../chat";
     }
 }
