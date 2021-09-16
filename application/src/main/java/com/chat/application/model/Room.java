27f68ad80c6dce40202cefe785a7b0aea20c5e7f
takeyamami <task8705@yahoo.co.jp>
2021-09-16 18:37:34 +0900

    add: modelを追加

---
+++ application/src/main/java/com/chat/application/model/Room.java
@@ -0,0 +1,14 @@
+package com.chat.application.model;
+
+public class Room {
+    private Integer rid;
+    private String roomName;
+
+    public Integer getRid() {
+        return this.rid;
+    }
+
+    public String getRoomName() {
+        return this.roomName;
+    }
+}
