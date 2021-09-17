8bf876ce90fe726c35d3501ad63f5e34a34d53da
takeyamami <task8705@yahoo.co.jp>
2021-09-17 19:17:04 +0900

    add: ユーザーモデルを追加

---
+++ application/src/main/java/com/chat/application/model/User.java
@@ -0,0 +1,19 @@
+package com.chat.application.model;
+
+public class User {
+    private Integer uid;
+    private String name;
+    private String email;
+
+    public Integer getUid() {
+        return this.uid;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
