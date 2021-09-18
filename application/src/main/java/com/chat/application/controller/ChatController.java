6a5eab33f88d9546a74fb71d15a5617062e2ee96
takeyamami <task8705@yahoo.co.jp>
2021-09-18 20:36:56 +0900

    add: modelの利用

--- application/src/main/java/com/chat/application/controller/ChatController.java
+++ application/src/main/java/com/chat/application/controller/ChatController.java
@@ -1,31 +1,37 @@
 package com.chat.application.controller;
 
+import com.chat.application.model.Room;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ChatController {
 
     @RequestMapping("/chat")
-    public String chat(ModelMap modelMap) {
+    public String chat(Model model) {
+        Room room = new Room(0, "");
+        model.addAttribute("room", room);
+
         return "chat";
     }
 
     @RequestMapping("/chat/{rid}")
-    public String chat(ModelMap modelMap, @PathVariable("rid") Integer rid) {
+    public String chat(Model model, @PathVariable("rid") Integer rid) {
         // Modelsを実装して、ダミーリストを作って画面に表示
-        modelMap.addAttribute("rid", rid);
+        Room room = new Room(rid, "てけてけ");
+        model.addAttribute("room", room);
 
         return "chat";
     }
 
     @PostMapping("/chat/action")
-    public String action(@RequestParam("rid")Integer rid,@RequestParam("message")String message,ModelMap modelMap) {
-        modelMap.addAttribute("rid", rid);
+    public String action(@RequestParam("rid")Integer rid,@RequestParam("message")String message,Model model) {
+        Room room = new Room(rid, "てけてけ");
+        model.addAttribute("room", room);
 
         return "redirect:/chat/" + rid;
     }
--- application/src/main/java/com/chat/application/model/Room.java
+++ application/src/main/java/com/chat/application/model/Room.java
@@ -4,6 +4,11 @@ public class Room {
     private Integer rid;
     private String roomName;
 
+    public Room(Integer rid, String roomName) {
+        this.rid = rid;
+        this.roomName = roomName;
+    }
+
     public Integer getRid() {
         return this.rid;
     }
