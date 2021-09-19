9cf1121c984a9706ea7b755d003cfad3b8473285
takeyamami <task8705@yahoo.co.jp>
2021-09-19 20:12:41 +0900

    add: validationの追加

--- application/src/main/java/com/chat/application/controller/LoginController.java
+++ application/src/main/java/com/chat/application/controller/LoginController.java
@@ -1,28 +1,30 @@
 package com.chat.application.controller;
 
+import com.chat.application.model.Login;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
     @RequestMapping("/")
-    public String index(ModelMap modelMap) {
+    public String index(@ModelAttribute Login login) {
         // ログイン情報があるかどうかで出し分けたい
         return "redirect:/login";
     }
 
     @RequestMapping("/login")
-    public String login(ModelMap modelMap) {
+    public String login(@ModelAttribute Login login) {
         return "login";
     }
 
-    @PostMapping("/login/action")
-    public String action(@RequestParam("email")String email,@RequestParam("password")String password) {
-        if (email.equals("") || password.equals("")) {
+    @PostMapping("/login")
+    public String action(@Validated @ModelAttribute Login login, BindingResult result) {
+        if (result.hasErrors()) {
             return "login";
         }
 
--- application/src/main/java/com/chat/application/model/Login.java
+++ application/src/main/java/com/chat/application/model/Login.java
@@ -1,2 +1,24 @@
-package com.chat.application.model;public class Login {
+package com.chat.application.model;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Login {
+    @NotBlank
+    @Email
+    @Size(max = 254)
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
