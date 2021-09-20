65fa5e04572f31c1f5043e6313143bf68507efa1
takeyamami <task8705@yahoo.co.jp>
2021-09-20 23:19:27 +0900

    update: setterを追加

--- application/src/main/java/com/chat/application/model/Login.java
+++ application/src/main/java/com/chat/application/model/Login.java
@@ -18,7 +18,15 @@ public class Login {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
