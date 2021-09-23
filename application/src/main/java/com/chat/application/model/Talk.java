47e04fcb174f7fcedd5831c1efa5f635aaee4c9a
takeyamami <task8705@yahoo.co.jp>
2021-09-23 17:02:47 +0900

    add: メッセージ登録用モデルを追加

---
+++ application/src/main/java/com/chat/application/model/Talk.java
@@ -0,0 +1,31 @@
+package com.chat.application.model;
+
+public class Talk {
+    private Integer rid;
+    private Integer uid;
+    private String message;
+
+    public Integer getRid() {
+        return rid;
+    }
+
+    public void setRid(Integer rid) {
+        this.rid = rid;
+    }
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
